c97a84f3bc86bcb4f8dddfc495cf6535f412427a
yirenong <yiren.ong.2021@scis.smu.edu.sg>
2023-11-13 01:15:58 +0800

    chatbot fix update.

--- chatbot.py
+++ chatbot.py
@@ -2,42 +2,77 @@ from flask import Flask, request, jsonify
 import openai
 import os
 
-# API-KEY
+# Set your OpenAI API key in an environment variable for security reasons
 openai.api_key = os.environ.get("OPENAI_KEY")
 
 app = Flask(__name__)
 
-def custom_chatbot_response(user_input):
-    print(request.json)
-    if "stress" in user_input.lower():
-        return "Why are you stressed? Do you need help?"
-    elif "suicidal" in user_input.lower() or "suicide" in user_input.lower():
-        # Future implementation: notify counselors
-        return ("If you are facing a mental health crisis, please call our Mental Health Helpline at 6389 2222 or "
-                "seek medical help at our 24-hour Emergency Services located in our hospital. All counselors will be notified as well.")
-    else:
-        return None  # Let the main function decide to use GPT-3
+# This will store the conversation history for each user in memory
+# Added a dummy user with user_id 'dummy_user' for testing
+chat_histories = {
+    'dummy_user': [
+        {"user": "Hello, how are you?", "bot": "I'm fine, thank you for asking! How can I assist you today?"}
+    ]
+}
 
-def get_response_from_gpt3(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=150
-    )
-    return response.choices[0].text.strip()
+def append_to_chat_history(user_id, exchange):
+    if user_id not in chat_histories:
+        chat_histories[user_id] = []
+    chat_histories[user_id].append(exchange)
+
+def get_chat_history(user_id):
+    return chat_histories.get(user_id, [])
+
+def create_prompt_with_context(chat_history, new_user_input):
+    prompt = f"User: {new_user_input}\nBot:"
+    for exchange in reversed(chat_history[-5:]):  # Adjust the slice to control context size
+        prompt = f"User: {exchange['user']}\nBot: {exchange['bot']}\n" + prompt
+    return prompt
+
+def get_response_from_gpt3(user_id, user_input):
+    chat_history = get_chat_history(user_id)
+    prompt_with_context = create_prompt_with_context(chat_history, user_input)
+    
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt_with_context,
+            max_tokens=150,
+            temperature=0.7,
+            top_p=1,
+            frequency_penalty=0.5,
+            presence_penalty=0.5,
+            stop=["\n"],  # Stop the response after one completion
+        )
+        gpt_response = response.choices[0].text.strip()
+
+        # Post-process the response to prevent repetitive answers
+        if gpt_response.lower() in ["i'm sorry to hear that.", "i'm sorry to hear that"]:
+            # Provide a more helpful follow-up or ask for more information
+            gpt_response = "I'm here for you. What's been on your mind lately?"
+
+        append_to_chat_history(user_id, {"user": user_input, "bot": gpt_response})
+        return gpt_response
+    except Exception as e:
+        print(f"Error during GPT-3 response generation: {e}")
+        return "I'm not sure how to respond to that. Can you rephrase?"
 
 @app.route('/chatbot', methods=['POST'])
 def chat_endpoint():
-    user_input = request.json.get('user_input')
-    response = custom_chatbot_response(user_input)
+    data = request.get_json()
+    # Default to using the dummy user if no user_id is provided
+    user_id = data.get('user_id', 'dummy_user')
+    user_input = data.get('user_input')
 
-    if response is None:  # If custom responses didn't match, use GPT-3
-        response = get_response_from_gpt3(user_input)
+    if not user_input:
+        return jsonify({'response': "User input is required."}), 400
 
-    print("Response to user:", response)  # Printing the response on server side
-
-    return jsonify({'response': response})
+    try:
+        response = get_response_from_gpt3(user_id, user_input)
+        return jsonify({'response': response})
+    except Exception as e:
+        print(f"Error in chat endpoint: {e}")
+        return jsonify({'response': "I'm experiencing difficulties. Please try again later."}), 500
 
 if __name__ == '__main__':
-    app.config['DEBUG'] = True
-    app.run(port=5001)
+    app.run(port=5000, debug=False)  # Set debug to False for production
